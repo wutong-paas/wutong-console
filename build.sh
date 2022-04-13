0bf6e1f9793dfd280f67a465d16bbf33581b8432
dingpeng24001 <dingpeng24001@talkweb.com.cn>
2022-04-13 11:17:32 +0800

    add github ci

---
+++ build.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# References:
+# 1. https://docs.docker.com/buildx/working-with-buildx/
+# 2. https://docs.docker.com/engine/reference/commandline/buildx/
+
+# Requirements:
+# 1. docker run --privileged --rm tonistiigi/binfmt --install all
+# 2. docker login dockerhub
+# 3. docker login myhuaweicloud-swr
+
+export NAMESPACE=wutong-console
+export VERSION=v1.0.0-stable
+docker buildx create --use --name consolebuilder
+docker buildx use consolebuilder
+docker buildx build --platform linux/amd64,linux/arm64 --push -t wutongpaas/${NAMESPACE}:${VERSION} -f Dockerfile.multiarch . 
+docker buildx build --platform linux/amd64,linux/arm64 --push -t swr.cn-southwest-2.myhuaweicloud.com/wutong/${NAMESPACE}:${VERSION} -f Dockerfile.multiarch . 
+# docker buildx rm consolebuilder
