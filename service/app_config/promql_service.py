29ddd853d93fa6ac63708c4609595984c25c7a3f
zhangdonglin <www.963649946@qq.com>
2022-04-15 10:51:33 +0800

    解决添加图表找不到路径问题

--- service/app_config/promql_service.py
+++ service/app_config/promql_service.py
@@ -4,6 +4,7 @@ import platform
 import subprocess
 from loguru import logger
 from exceptions.main import AbortRequest
+from core.setting import settings
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
@@ -14,7 +15,7 @@ class PromQLService(object):
         """
         Add service_id label, or replace illegal service_id label
         """
-        promql_parser = BASE_DIR + "/bin/" + platform.system().lower() + "/promql-parser"
+        promql_parser = settings.BASE_DIR + "/bin/" + platform.system().lower() + "/promql-parser"
         c = subprocess.Popen([os.getenv("PROMQL_PARSER", promql_parser), "--component_id", component_id],
                              stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE,
