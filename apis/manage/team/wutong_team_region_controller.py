1c7af0fa08eed6e87585a93393a717a8a9a5de03
zhangdonglin <www.963649946@qq.com>
2022-05-27 16:17:14 +0800

    解决文件管理不能新建文件问题及增加访问权限管理

--- apis/manage/team/wutong_team_region_controller.py
+++ apis/manage/team/wutong_team_region_controller.py
@@ -317,11 +317,12 @@ async def file_manager(
         request: Request,
         service_id: Optional[str] = None,
         url: Optional[str] = None,
+        user=Depends(deps.get_current_user),
         session: SessionClass = Depends(deps.get_session)) -> Any:
     try:
-        if request.method != "GET":
+        try:
             body = await request.json()
-        else:
+        except:
             body = {}
         service = service_repo.get_service_by_service_id(session, service_id)
         response = await remote_app_client.proxy(session, request,
