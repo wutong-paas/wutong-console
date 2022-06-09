d638e26912473adbfce7ef1304c89a2d839c6c11
zhangdonglin <www.963649946@qq.com>
2022-06-09 17:27:59 +0800

    修改helm应用安装接口

--- apis/manage/application/application_controller.py
+++ apis/manage/application/application_controller.py
@@ -79,7 +79,7 @@ async def create_app(params: TeamAppCreateRequest,
         return JSONResponse(result, status_code=result["code"])
 
     k8s_app = params.k8s_app
-    if params.app_name:
+    if not k8s_app and params.app_name:
         k8s_app = params.app_name
     if k8s_app and not is_qualified_name(k8s_app):
         raise ErrQualifiedName(msg_show="应用英文名称只能由小写字母、数字或“-”组成，并且必须以字母开始、以数字或字母结尾")
