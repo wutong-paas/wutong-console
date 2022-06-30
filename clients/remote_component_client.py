34b693c00289acfd9af7dd3faf170fcb9f8cb788
zhangdonglin-90528 <zhangdonglin@talkweb.com.cn>
2022-06-30 11:14:11 +0800

    关闭keep_alive

--- clients/remote_component_client.py
+++ clients/remote_component_client.py
@@ -19,7 +19,7 @@ class RemoteComponentClient(ApiBaseHttpClient):
 
     def __init__(self, *args, **kwargs):
         ApiBaseHttpClient.__init__(self, *args, **kwargs)
-        self.default_headers = {'Connection': 'keep-alive', 'Content-Type': 'application/json'}
+        self.default_headers = {'Connection': 'close', 'Content-Type': 'application/json'}
 
     def _set_headers(self, token):
 
