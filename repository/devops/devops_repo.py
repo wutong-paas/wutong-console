cb2607ef037059d4d626ef15657a33abbf74184c
zhangdonglin <zhangdonglin@talkweb.com.cn>
2022-10-20 16:34:37 +0800

    解决devops与pass环境变量冲突导致删除失败问题

--- repository/devops/devops_repo.py
+++ repository/devops/devops_repo.py
@@ -47,7 +47,7 @@ class DevopsRepository:
         )).scalars().first()
 
         if not env:
-            return general_message(400, "delete env not found", "需要删除的环境变量未找到")
+            return general_message(200, "delete env not found", "需要删除的环境变量未找到")
         env_var_service.delete_env_by_env_id(session=session, tenant=tenant, service=service, env_id=env.ID,
                                              user_name=user.nick_name)
         return general_message(200, "success", "删除成功")
