9b3c3b523551cde8a7efac6e66b75b04dee4742e
zhangdonglin <zhangdonglin@talkweb.com.cn>
2022-12-22 17:00:26 +0800

    解决源码组件构建源获取分支失败问题

--- core/utils/custom_config.py
+++ core/utils/custom_config.py
@@ -2,18 +2,56 @@
 import json
 import logging
 
+from sqlalchemy import select
+from core.setting import settings
+from models.teams import ConsoleSysConfig
+
 logger = logging.getLogger('default')
 
+
+class MemcachedCli(object):
+    def __init__(self):
+        pass
+
+    def getKey(self, key):
+        pass
+
+    def setKey(self, key, value):
+        pass
+
+
+mcli = MemcachedCli()
 configKey = "SYS_C_F_K"
 
 
 class ConfigCenter(object):
 
-    def loadfromDB(self):
+    def __init__(self):
+        self.session = None
+
+    def __getattr__(self, name):
+        configs = self.configs(self.session)
+        if name in configs:
+            return configs[name]
+        else:
+            if hasattr(settings, name):
+                return getattr(settings, name)
+            else:
+                return None
+
+    def configs(self, session):
+        return self.loadfromDB(session)
+
+    def reload(self, session):
+        mcli.setKey(configKey, json.dumps(self.loadfromDB(session)))
+
+    def loadfromDB(self, session):
 
         objects = {}
         # 查询启用的配置
-        configs = ConsoleSysConfig.objects.filter(enable=True)
+        configs = session.execute(select(ConsoleSysConfig).where(
+            ConsoleSysConfig.enable == 1
+        )).scalars().all()
         for config in configs:
             if config.type == "int":
                 c_value = int(config.value)
@@ -36,6 +74,9 @@ class ConfigCenter(object):
             objects[config.key] = c_value
         mcli.setKey(configKey, json.dumps(objects))
         return objects
+
+    def init_session(self, session):
+        self.session = session
 
 
 custom_config = ConfigCenter()
