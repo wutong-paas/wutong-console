30758a960b66be2181d8ee4e33b8c1e5a4803700
HuangTao <huangtao23993@talkweb.com.cn>
2023-03-21 14:44:02 +0800

    资源删除功能修改

--- service/env_delete_service.py
+++ service/env_delete_service.py
@@ -12,13 +12,14 @@ from service.app_actions.app_manage import app_manage_service
 from service.base_services import base_service
 from service.plugin_service import plugin_service
 from service.region_service import region_services
+from service.tenant_env_service import env_services
 
 
 def logic_delete_by_env_id(session, user_nickname, env):
     # 查询环境下全部应用，停用全部组件
     stop_env_resource()
     # 环境、应用、组件 标记为逻辑删除
-    logic_delete_env()
+    logic_delete_env(session=session, env=env, user_nickname=user_nickname)
     logger.info("逻辑删除环境资源")
 
 
@@ -26,7 +27,8 @@ def stop_env_resource():
     logger.info("")
 
 
-def logic_delete_env(session, env, region_name, user_nickname):
+
+def logic_delete_env(session, env, user_nickname):
     # start delete
-    region_config = region_repo.get_enterprise_region_by_region_name(session, region_name)
+    region_config = region_repo.get_enterprise_region_by_region_name(session, env.region_name)
     ignore_cluster_resource = True
