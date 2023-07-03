cdd5648975ff6756d3053b7b8d5b44851b54b78b
zhangdonglin <zhangdonglin@talkweb.com.cn>
2023-07-03 11:17:35 +0800

    更新拓扑图资源

--- weavescope/www/weavescope/vendors.js
+++ weavescope/www/weavescope/vendors.js
@@ -90,7 +90,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + chunkId + "-" + {"0":"87c50a8b0f9ec0d92e49"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + chunkId + "-" + {"0":"53984396e290ea97008f"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
